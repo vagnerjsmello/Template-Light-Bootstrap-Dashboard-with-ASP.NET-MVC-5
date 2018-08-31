8cb0c2970199e01869acad53a3ac8ba8496fdf84
vagnerjsmello <vagnerjsmello@gmail.com>
2018-08-30 22:12:21 -0300

    Add Views Account, Configuration, Feed, Profile and razor @Url.Action

---
+++ VMSocialNetwork.MVC.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMSocialNetwork.MVC.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ VMSocialNetwork.MVC.WebApp/Controllers/FeedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMSocialNetwork.MVC.WebApp.Controllers
+{
+    public class FeedController : Controller
+    {
+        // GET: Feed
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
